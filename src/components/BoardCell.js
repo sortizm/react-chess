e97e3508320763bf07e5ae04cc8f7094b31e438a
Steven Ortiz <stevenortiz.es@gmail.com>
2020-02-04 20:55:25 +0000

    Add capturing piece functionality

--- src/components/BoardCell.js
+++ src/components/BoardCell.js
@@ -21,13 +21,28 @@ const BoardCell = ({pieces, row, col, selectedCell, setSelectedCell, updatePiece
     const cellStyle = "cell " + (row%2 === col%2 ? 'black-cell' : 'white-cell') +
         (isPieceSelected? ' highlighted' : '');
 
-    const onSelected = () => {
+    const movePiece = (piece, row, col) => {
+        updatePiece(piece.key, {...piece, col: col, row: row});
+    };
+
+    const capturePiece = (piece) => {
+        updatePiece(piece.key, {...piece, col: -1, row: -1});
+    };
+
+    const resetSelected = () => {
+        setSelectedCell([]);
+    };
+
+    const onCellClick = () => {
         if (isPieceSelected) {
-            setSelectedCell([]);
+            resetSelected();
         } else {
             if (pieceInSelectedCell) {
-                updatePiece(pieceInSelectedCell.key, {...pieceInSelectedCell, col: col, row: row});
-                setSelectedCell([]);
+                movePiece(pieceInSelectedCell, row, col);
+                resetSelected();
+                if (pieceInCell) {
+                    capturePiece(pieceInCell);
+                }
             } else {
                 setSelectedCell([row, col]);
             }
@@ -37,7 +52,7 @@ const BoardCell = ({pieces, row, col, selectedCell, setSelectedCell, updatePiece
     const imageInCell = pieceInCell ? <img key={pieceInCell.key} src={pieceInCell.image}/>
         : <img/>;
     return (
-        <div className={cellStyle} onClick={onSelected}>
+        <div className={cellStyle} onClick={onCellClick}>
             {imageInCell}
         </div>
     )
